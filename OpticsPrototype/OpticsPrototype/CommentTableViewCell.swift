c024bcfbbbd0b6bb4c19d65db570b5ff77a28407
SwithFr <contact@swith.fr>
2015-11-06 00:39:30 +0100

    add comment cell

---
+++ OpticsPrototype/OpticsPrototype/CommentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentTableViewCell.swift
+//  OpticsPrototype
+//
+//  Created by Jérémy Smith on 05/11/2015.
+//  Copyright © 2015 Jérémy Smith. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var comment: UITextView!
+    @IBOutlet weak var authorAvatar: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
