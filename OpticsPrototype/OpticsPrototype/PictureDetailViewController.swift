56ac4c7258c6f7ca747903547e77c5e26f614055
SwithFr <contact@swith.fr>
2015-12-16 17:04:25 +0100

    many things (oh very useful message !)

--- OpticsPrototype/OpticsPrototype/PictureDetailViewController.swift
+++ OpticsPrototype/OpticsPrototype/PictureDetailViewController.swift
@@ -11,51 +11,73 @@ import UIKit
 class PictureDetailViewController: UIViewController, UITableViewDataSource {
     
     var currentPicture: JSON!
-    let BorderColor = UIColor(red:0.10, green:0.12, blue:0.16, alpha:1.0)
+    var comments: [JSON] = []
     
+    @IBOutlet weak var addCommentBtn: UIButton!
+    @IBOutlet weak var myTableView: UITableView!
     @IBOutlet weak var picture: UIImageView!
     @IBOutlet weak var author: UILabel!
     @IBOutlet weak var time: UILabel!
-    @IBOutlet weak var commentsCount: UILabel!
     @IBOutlet weak var authorPictureAvatar: UIImageView!
     @IBOutlet weak var deleteBtn: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let decodedData = NSData(base64EncodedString: String(currentPicture["image"]), options: NSDataBase64DecodingOptions(rawValue: 0))
-        let decodedimage = UIImage(data: decodedData!)
+        let decodedData = NSData( base64EncodedString: String( currentPicture[ "image" ] ), options: NSDataBase64DecodingOptions( rawValue: 0 ) )
+        let decodedimage = UIImage( data: decodedData! )
         
         picture.image = decodedimage! as UIImage
-        author.text = currentPicture["author"].string
-        time.text = Date.ago(currentPicture["date"].string!)
-        commentsCount.text = String(currentPicture["comments"].int!)
-        formatRoundedImage(authorPictureAvatar, radius: 20, color: BorderColor, border: 1.5)
-        //authorPictureAvatar.image = UIImage(named: "moi.png")
-        authorPictureAvatar.image = getImageFromUrl(currentPicture["avatar"].string!)
+        author.text = currentPicture[ "author" ].string
+        time.text = Date.ago( currentPicture[ "date" ].string! )
         
-        if !User.isOwner(currentPicture[ "user_id" ]) {
+        formatBtn( addCommentBtn )
+        formatRoundedImage(authorPictureAvatar, radius: 20, color: black, border: 1.5)
+        authorPictureAvatar.image = getImageFromUrl( currentPicture[ "avatar" ].string! )
+        
+        if !User.isOwner( currentPicture[ "user_id" ] ) {
             self.navigationItem.rightBarButtonItem = nil
         }
-        //self.navigationItem.title = currentPicture["title"].string
+    }
+    
+    var messageFrame = UIView()
+    var activityIndicator = UIActivityIndicatorView()
+    var strLabel = UILabel()
+    
+    override func viewDidAppear(animated: Bool) {
+        showLoader( "Chargement" )
+        let rc = RestCaller()
+        rc.setParams( [ "pictureid": String( currentPicture[ "id" ] ) ] )
+        rc.get( "comments", authenticate: true) {
+            error, data in
+            
+            if error != nil {
+                print( "Error when getting comments" )
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.hideLoader()
+                self.setAndReloadData( data )
+            }
+        }
+    }
+    
+    func setAndReloadData(data: NSData) {
+        let coms = JSON( data: data )
+        self.comments = coms[ "data" ].arrayValue
+        self.myTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    var comments = [
-        ["author": "Daniel", "comment": "Un super commentaire", "authorAvatar": "fake.png"],
-        ["author": "Delphine", "comment": "Un super commentaire", "authorAvatar": "fake.png"],
-        ["author": "Adrien", "comment": "Grillades !", "authorAvatar": "fake.png"]
-    ]
-    
     @IBAction func deleteBtnDidTouch(sender: AnyObject) {
         let popView = UIAlertController( title: "Supprimer cette photo", message: "Voulez vous vraiment supprimer cette photo ?", preferredStyle: .Alert )
         
         let defaultAction = UIAlertAction( title: "Supprimer", style: .Destructive) {
             ( alert: UIAlertAction! ) in
-            Picture.delete( String(self.currentPicture[ "id" ]) ) {
+            Picture.delete( String(self.currentPicture[ "id" ] ) ) {
                 Navigator.goBack( self )
             }
         }
@@ -69,17 +91,24 @@ class PictureDetailViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("commentCell", forIndexPath: indexPath) as! CommentTableViewCell
-        let comment = comments[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier( "commentCell", forIndexPath: indexPath ) as! CommentTableViewCell
+        let comment = comments[ indexPath.row ]
         
-        tableView.separatorColor = UIColor(red:0, green:0, blue:0, alpha:0)
+        tableView.separatorColor = grey
         
-        cell.author.text = comment["author"]! as String
-        cell.comment.text = comment["comment"]! as String
-        formatRoundedImage(cell.authorAvatar, radius: 20, color: BorderColor, border: 1.5)
-        cell.authorAvatar.image = UIImage(named: comment["authorAvatar"]! as String)
+        cell.author.text = comment[ "author" ].string
+        cell.comment.text = comment[ "comment" ].string
+        formatRoundedImage( cell.authorAvatar, radius: 20, color: black, border: 1.5 )
+        cell.authorAvatar.image = getImageFromUrl( String( comment[ "avatar" ] ) )
         
         return cell
-        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "addCommentSegue" {
+            let commentViewController = segue.destinationViewController as! AddCommentViewController
+            commentViewController.sPictureid = String( currentPicture[ "id" ] )
+            commentViewController.sEventId = currentPicture[ "event_id" ].string
+        }
     }
 }
