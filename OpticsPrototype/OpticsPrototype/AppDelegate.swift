18cc64f873047cce7254be08d1adcaa0f1b474ea
SwithFr <contact@swith.fr>
2015-12-16 16:57:34 +0100

    hide back button title

--- OpticsPrototype/OpticsPrototype/AppDelegate.swift
+++ OpticsPrototype/OpticsPrototype/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.sharedApplication().statusBarStyle = .LightContent
+        let barAppearace = UIBarButtonItem.appearance()
+        barAppearace.setBackButtonTitlePositionAdjustment(UIOffsetMake(0, -60), forBarMetrics:UIBarMetrics.Default)
         return true
     }
 
