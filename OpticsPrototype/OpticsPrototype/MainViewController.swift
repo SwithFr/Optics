491747a7c4e01cb9a475cdb2ebadd5669d0804bc
SwithFr <contact@swith.fr>
2015-12-16 17:00:34 +0100

    add UIViewController extension

--- OpticsPrototype/OpticsPrototype/MainViewController.swift
+++ OpticsPrototype/OpticsPrototype/MainViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class MainViewController: UINavigationController {
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool)
+    {
         
         if !User.isAuthenticated() {
             let loginVC = self.storyboard?.instantiateViewControllerWithIdentifier( "loginScreen" )
@@ -19,4 +20,39 @@ class MainViewController: UINavigationController {
         
     }
 
+}
+
+extension UIViewController {
+    
+    func showLoader(msg: String)
+    {
+        let actInd: UIActivityIndicatorView = UIActivityIndicatorView()
+        let message = UILabel( frame: CGRect( x: 50, y: 0, width: 200, height: 50 ) )
+        let container = UIView( frame: CGRect( x: view.frame.midX - 90, y: view.frame.midY - 25 , width: 180, height: 50 ) )
+        
+        actInd.activityIndicatorViewStyle = .White
+        message.text = msg
+        message.textColor = UIColor.whiteColor()
+        container.layer.cornerRadius = 15
+        container.backgroundColor = UIColor( white: 0, alpha: 0.7 )
+        actInd.frame = CGRect( x: 0, y: 0, width: 50, height: 50 )
+        actInd.startAnimating()
+        container.addSubview( actInd )
+        container.addSubview( message )
+        container.tag = 1000
+        view.addSubview( container )
+        actInd.startAnimating()
+    }
+    
+    func hideLoader()
+    {
+        if let viewWithTag = self.view.viewWithTag(1000) {
+            viewWithTag.removeFromSuperview()
+        }
+    }
+//    
+//    func hideBackButonTitle()
+//    {
+//        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.Plain, target:nil, action:nil)
+//    }
 }
